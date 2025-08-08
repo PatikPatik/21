eccab9fc102e56e4fe220d5ca27df0d3bac71842
PatikPatik <160071263+PatikPatik@users.noreply.github.com>
2025-08-08 13:30:05 +0300

    Update health.py

--- app/utils/health.py
+++ app/utils/health.py
@@ -2,7 +2,9 @@ from aiohttp import web
 
 def make_health_app() -> web.Application:
     app = web.Application()
+
     async def healthz(_):
         return web.Response(text="ok")
+
     app.add_routes([web.get("/healthz", healthz)])
     return app
